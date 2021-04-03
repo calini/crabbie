d3b9bd71010fc69f8d40de39f1acbd4b8548751a
Calin Ilie <calin@ilie.io>
2021-04-03 21:51:31 +0100

    Implement Jackbox-like alphanumeric room code generation

--- pkg/strings/strings.go
+++ pkg/strings/strings.go
@@ -1,14 +1,28 @@
 package strings
 
 import (
-	"encoding/hex"
 	"math/rand"
+	"strings"
+	"time"
 )
 
+func init() {
+	rand.Seed(time.Now().Unix())
+}
+
 func GenerateCode(length int) string {
+	charSet := "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	var output strings.Builder
+
+	for i := 0; i < length; i++ {
+		random := rand.Intn(len(charSet))
+		randomChar := charSet[random]
+		output.WriteString(string(randomChar))
+	}
+
 	b := make([]byte, length)
 	if _, err := rand.Read(b); err != nil {
 		return ""
 	}
-	return hex.EncodeToString(b)
+	return output.String()
 }

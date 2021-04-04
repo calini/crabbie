e1c5ce37a5425f5e4ee5815e88ffcbc2fcb8a072
Andrei Tuta <tuta.andrei96@gmail.com>
2021-04-04 13:59:03 +0100

    Change struct order and formatting

--- main.go
+++ main.go
@@ -27,19 +27,22 @@ func main() {
 }
 
 func GetNewRoom(c *gin.Context) {
+
 	code := strings.GenerateCode(CODE_LENGTH)
+
 	activeRooms[code] = Room{code, "1", []Player{}, []string{}}
 	response := fmt.Sprintf("Here's your new room: %s, %s", activeRooms[code].Code, activeRooms[code].RoomType)
 	c.String(http.StatusAccepted, response)
 }
 
 func CreateNewUser(c *gin.Context) {
+
 	room, found := activeRooms[c.Param("code")]
+
 	if found {
 		player := Player{c.Param("user_name"), []string{}}
 		room.Players = append(room.Players, player)
 		activeRooms[room.Code] = room
-		fmt.Print(room)
 		c.String(http.StatusOK, player.Name)
 	} else {
 		c.String(http.StatusNotFound, "Could not add user for room!")
@@ -63,14 +66,14 @@ func GetRoom(c *gin.Context) {
 	}
 }
 
+type Player struct {
+	Name    string   `json:"player_name"`
+	Answers []string `json:"player_answers"`
+}
+
 type Room struct {
 	Code      string   `json:"room_code"`
 	RoomType  string   `json:"room_type"`
 	Players   []Player `json:"players_in_room"`
 	Questions []string `json:"sets_of_questions"`
-}
-
-type Player struct {
-	Name    string   `json:"player_name"`
-	Answers []string `json:"player_answers"`
 }
